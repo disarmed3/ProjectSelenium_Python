e8656f0f6a55b37217137d6137334d76d21241f2
Tasos <disarmed3@gmail.com>
2024-06-15 08:57:46 +0300

    Deus Ex Machina version 1.1

--- main.py
+++ main.py
@@ -35,23 +35,21 @@ def test_qa_engineer_link(driver):
 
 def test_application_link(driver):
     WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, "//div[contains(@class,'fab-Card fab-Card--sizeFull fab-Card--withoutBottomGradient')]//span[contains(@class,'jss-f76')][normalize-space()='Apply for This Job']"))).click()
+        EC.element_to_be_clickable((By.XPATH, "//div[contains(@class,'fab-Card fab-Card--sizeFull fab-Card--withoutBottomGradient')]//button[contains(@type,'button')]"))).click()
     text = driver.page_source
     assert "Apply for this Position" in text, "Expected 'Apply for this Position' in page source"
 
 def test_fill_info(driver):
-    name = "Anastasios"
-    surname = "Athanasiadis"
-    email = "t.athanasiadis.test@gmail.com"
-    phone = "693*******"
-    linkedin = "https://www.linkedin.com/in/aathanasiadis/"
-    github = "https://github.com/disarmed3"
+    fields = {
+        "firstName": "Anastasios",
+        "lastName": "Athanasiadis",
+        "email": "t.athanasiadis.test@gmail.com",
+        "phone": "693*******",
+        "websiteUrl": "https://github.com/disarmed3",
+        "linkedinUrl": "https://www.linkedin.com/in/aathanasiadis/"
+    }
 
-    driver.find_element(By.ID, "firstName").send_keys(name)
-    driver.find_element(By.ID, "lastName").send_keys(surname)
-    driver.find_element(By.ID, "email").send_keys(email)
-    driver.find_element(By.ID, "phone").send_keys(phone)
-    driver.find_element(By.ID, "websiteUrl").send_keys(github)
-    driver.find_element(By.ID, "linkedinUrl").send_keys(linkedin)
+    for field_id, value in fields.items():
+        driver.find_element(By.ID, field_id).send_keys(value)
 
     driver.find_element(By.CLASS_NAME, "CandidateField__checkboxRequiredIndicator").click()
